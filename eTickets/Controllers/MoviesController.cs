49e624616660826226dd882d370f6e34c39708dc
Marcos Costa <90783203+marcoscariaga@users.noreply.github.com>
2023-09-12 20:15:39 -0300

    Subida

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -29,6 +30,17 @@ namespace eTickets.Controllers
         {
             var movieDetail = await _service.GetMovieByIdAsync(id);
             return View(movieDetail);
+        }
+
+        //GET: Movies/Create
+        public async Task<IActionResult> Create()
+        {
+            var movieDropDownData = await _service.GetNewMovieDropDownValues();
+            ViewBag.Cinemas = new SelectList(movieDropDownData.Cinemas, "Id", "Name");
+            ViewBag.Producers = new SelectList(movieDropDownData.Producers, "Id", "FullName");
+            ViewBag.Actors = new SelectList(movieDropDownData.Actors, "Id", "FullName");
+
+            return View();
         }
     }
 }
---
+++ eTickets/Data/ViewModels/NewMovieVM.cs
@@ -0,0 +1,54 @@
+﻿using eTickets.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class NewMovieVM
+    {
+        [Display(Name = "Movie Name")]
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Movie Description")]
+        [Required(ErrorMessage = "Description is Required")]
+        public string Description { get; set; }
+
+        [Display(Name = "Price Name in US$")]
+        [Required(ErrorMessage = "Price is Required")]
+        public double Price { get; set; }
+
+        [Display(Name = "Movie Poster URL")]
+        [Required(ErrorMessage = "Movie Poster URL is Required")]
+        public string ImageURL { get; set; }
+
+        [Display(Name = "Movie Start Date")]
+        [Required(ErrorMessage = "Movie StartDate is Required")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Movie End Date")]
+        [Required(ErrorMessage = "Movie End Date is Required")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Select a category")]
+        [Required(ErrorMessage = "Movie Category is Required")]
+        public MovieCategory MovieCategory { get; set; }
+
+        //Relationships
+        [Display(Name = "Select Actor(s)")]
+        [Required(ErrorMessage = "Movie Actor(s) is Required")]
+        public List<int> ActorIds { get; set; }
+
+        [Display(Name = "Select a Cinema")]
+        [Required(ErrorMessage = "Movie Cinema is Required")]
+        public int CinemaId { get; set; }
+
+        [Display(Name = "Select a Producer")]
+        [Required(ErrorMessage = "Movie Producer is Required")]
+        public int ProducerId { get; set; }
+    }
+}
